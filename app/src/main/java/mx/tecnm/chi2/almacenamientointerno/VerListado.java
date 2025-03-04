102ff65227522da9adfb8e24e686a1d849bb4b15
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-03-04 15:52:53 -0600

    Se agregan funcionalidades a VerListado

--- app/src/main/java/mx/tecnm/chi2/almacenamientointerno/VerListado.java
+++ app/src/main/java/mx/tecnm/chi2/almacenamientointerno/VerListado.java
@@ -1,6 +1,7 @@
 package mx.tecnm.chi2.almacenamientointerno;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class VerListado extends AppCompatActivity {
 
+    ListView listView_listado;
+    String[] productos;
+    FileInputStream fileInputStream;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,30 @@ public class VerListado extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //vincular objetos
+        listView_listado = findViewById(R.id.listView_listado);
+        obtenerProductos();
+
+    }//onCreate
+
+    public void obtenerProductos(){
+        try {
+            fileInputStream = openFileInput("productos");
+            StringBuffer stringBuffer = new StringBuffer();
+            int i;
+            while((i = fileInputStream.read()) != -1){
+                stringBuffer.append((char) i);
+            }
+            fileInputStream.close();
+
+            productos = stringBuffer.toString().split("\n");
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+}//verListado
